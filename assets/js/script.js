6c3054a3f71175105f026c61321c7e1e09ec2194
ezzelden.work <ezzelden.work@gmail.com>
2023-01-30 15:38:18 +0200

    new style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  if ($("html").attr("dir") == "rtl") {
+    var dir_lang = true;
+  } else {
+    var dir_lang = false;
+  }
 
-  
+  $(".common-slider").slick({
+    slidesToShow: 4,
+    prevArrow: "#blog-slider-prev",
+    nextArrow: "#blog-slider-next",
+    infinite: false,
+    rtl: dir_lang,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 772,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
