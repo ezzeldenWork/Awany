2584635d0a22aca5f504128f500ad01cfde54aed
ezzelden.work <ezzelden.work@gmail.com>
2023-01-30 16:16:59 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,4 +28,27 @@ $(document).ready(function () {
       },
     ],
   });
+  $(".new-slider").slick({
+    slidesToShow: 3,
+    prevArrow:
+      "<div class='time-arrow left-arrow'><i class='fas fa-chevron-left'></i></div>",
+    nextArrow:
+      "<div class='time-arrow right-arrow'><i class='fas fa-chevron-right'></i></div>",
+    infinite: false,
+    rtl: dir_lang,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 772,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
